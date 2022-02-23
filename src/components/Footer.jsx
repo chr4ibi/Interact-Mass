13f67dbfe7fcfcb7d9231ef52f3f822d72975b11
CHR4IBI <chraibialipro@gmail.com>
2022-02-23 12:34:12 +0100

    small changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,24 +5,41 @@ import styled from 'styled-components'
 const Footer = () => {
     return (
         <FooterContainer>
-            <FooterText>Copyright 2021 All Rights Reserved Interact Mass</FooterText>
+            <FooterText>Copyright 2021 | All Rights Reserved | Interact Mass</FooterText>
+            <FooterText>Made with heart by <PortfolioLink target="_blank" href="#">chr4ibi</PortfolioLink></FooterText>
         </FooterContainer>
     )
 }
 
 const FooterContainer = styled.div`
   width: 100%;
-  height: 80px;
   background-color: #0070f3;
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
+  padding: 10px;
 `;
 
 const FooterText = styled.p`
   color: #fff;
   font-size: 18px;
   text-align: center;
+  margin: 5px 0px;
 `;
+
+const PortfolioLink = styled.a`
+  color: pink;
+  font-weight: bold;
+  cursor: pointer;
+  text-decoration: none;
+  
+
+  &:hover {
+    text-decoration: underline;
+    transition: ease-in-out 0.3s;
+    color: #000;
+  }
+`
 
 export default Footer
