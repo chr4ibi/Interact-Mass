d0b7fa93be095c3ab8b85ce29a166ba4fc11dd18
CHR4IBI <chraibialipro@gmail.com>
2022-03-26 12:55:06 +0100

    galery images update

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,4 +1,11 @@
 // Import assets
+import bluesweat1 from "../assets/images/bluesweat1.jpeg";
+import brownsweat1 from "../assets/images/brownsweat1.jpeg";
+import blacksweat1 from "../assets/images/blacksweat1.jpeg";
+import purplesweat1 from "../assets/images/purplesweat1.jpeg";
+import about1 from "../assets/illustrations/about1.svg";
+import about2 from "../assets/illustrations/about2.svg";
+import about3 from "../assets/illustrations/about3.svg";
 import profilepic from "../assets/images/profilepic.png";
 import missour1 from "../assets/images/missour1.jpeg";
 import missour2 from "../assets/images/missour2.jpeg";
@@ -8,13 +15,38 @@ import missour5 from "../assets/images/missour5.jpeg";
 import missour6 from "../assets/images/missour6.jpeg";
 import missour7 from "../assets/images/missour7.jpeg";
 import missour8 from "../assets/images/missour8.jpeg";
-import bluesweat1 from "../assets/images/bluesweat1.jpeg";
-import brownsweat1 from "../assets/images/brownsweat1.jpeg";
-import blacksweat1 from "../assets/images/blacksweat1.jpeg";
-import purplesweat1 from "../assets/images/purplesweat1.jpeg";
-import about1 from '../assets/illustrations/about1.svg'
-import about2 from '../assets/illustrations/about2.svg'
-import about3 from '../assets/illustrations/about3.svg'
+import mealbasket1 from "../assets/images/mealbasket1.jpeg";
+import mealbasket2 from "../assets/images/mealbasket2.jpeg";
+import mealbasket3 from "../assets/images/mealbasket3.jpeg";
+import mealbasket4 from "../assets/images/mealbasket4.jpeg";
+import mealbasket5 from "../assets/images/mealbasket5.jpeg";
+import mealbasket6 from "../assets/images/mealbasket6.jpeg";
+import mealbasket7 from "../assets/images/mealbasket7.jpeg";
+import mealbasket8 from "../assets/images/mealbasket8.jpeg";
+import freetutoring1 from "../assets/images/freetutoring1.jpeg";
+import freetutoring2 from "../assets/images/freetutoring2.jpeg";
+import freetutoring3 from "../assets/images/freetutoring3.jpeg";
+import freetutoring4 from "../assets/images/freetutoring4.jpeg";
+import freetutoring5 from "../assets/images/freetutoring5.jpeg";
+import freetutoring6 from "../assets/images/freetutoring6.jpeg";
+import freetutoring7 from "../assets/images/freetutoring7.jpeg";
+import freetutoring8 from "../assets/images/freetutoring8.jpeg";
+import beachcleaning1 from "../assets/images/beachcleaning1.jpeg";
+import beachcleaning2 from "../assets/images/beachcleaning2.jpeg";
+import beachcleaning3 from "../assets/images/beachcleaning3.jpeg";
+import beachcleaning4 from "../assets/images/beachcleaning4.jpeg";
+import beachcleaning5 from "../assets/images/beachcleaning5.jpeg";
+import beachcleaning6 from "../assets/images/beachcleaning6.jpeg";
+import beachcleaning7 from "../assets/images/beachcleaning7.jpeg";
+import beachcleaning8 from "../assets/images/beachcleaning8.jpeg";
+import shortfilm1 from "../assets/images/shortfilm1.jpeg";
+import shortfilm2 from "../assets/images/shortfilm2.jpeg";
+import shortfilm3 from "../assets/images/shortfilm3.jpeg";
+import shortfilm4 from "../assets/images/shortfilm4.jpeg";
+import shortfilm5 from "../assets/images/shortfilm5.jpeg";
+import shortfilm6 from "../assets/images/shortfilm6.jpeg";
+import shortfilm7 from "../assets/images/shortfilm7.jpeg";
+import shortfilm8 from "../assets/images/shortfilm8.jpeg";
 
 export const aboutData = [
   {
@@ -26,7 +58,7 @@ export const aboutData = [
       p1: "The Interact is a club for high school students, whose purpose is to teach the importance of community service. The club has many branches around the world. ",
       p2: "Since 1962, when it was founded by Rotary International, Interact has conducted humanitarian projects ranging from the smallest to the largest. Rotary engages its members in causes that strengthen international relations, improve lives, and create a better world in support of our peace efforts.",
       p3: "Interact Massignon is the branch of this club at LFILM in District 9010. We are a group of 66 students involved in the issues that Morocco must overcome on a humanitarian level.",
-    }
+    },
   },
   {
     image: about2,
@@ -37,7 +69,7 @@ export const aboutData = [
       p1: "Our values are humanity, benevolence and sharing. Through our various actions, we are committed to helping as many people as possible in need.",
       p2: "We are also and above all a family, which shares the same values and appreciates seeing the bonds created between our members.",
       p3: "We are also and above all a family, which shares the same values and appreciates seeing the bonds created between our members.",
-    }
+    },
   },
   {
     image: about3,
@@ -48,11 +80,9 @@ export const aboutData = [
       p1: "The Interact is our way as teenagers to make a difference in our society. Every day, every afternoon, every child's smile fills our hearts with pride and joy for doing something that may seem simple to us but means so much to those less fortunate. ",
       p2: "That is why our goal has always been to defend noble causes that we all hold dear, hoping to make the Morocco of tomorrow a better country for its poorest inhabitants. ",
       p3: "We, the generation of tomorrow, must achieve this goal to make our country and our world a better place.",
-    }
+    },
   },
-
-
-]
+];
 
 export const memberList = [
   {
@@ -73,16 +103,6 @@ export const memberList = [
   {
     name: "Selma Tiouale",
     role: "Treasurer",
-    profilePic: profilepic,
-  },
-  {
-    name: "Lina Derrabi",
-    role: "Member",
-    profilePic: profilepic,
-  },
-  {
-    name: "Kenza Bouzoubaa",
-    role: "Member",
     profilePic: profilepic,
   },
   {
@@ -339,53 +359,68 @@ export const actionList = [
     ],
   },
   {
-    title: "Charity Action : Preparation and distribution of meal baskets in Casablanca",
+    title:
+      "Charity Action : Preparation and distribution of meal baskets in Casablanca",
     date: "December 2021",
     desc: "",
-    images : [],
+    images: [
+      mealbasket1,
+      mealbasket2,
+      mealbasket3,
+      mealbasket4,
+      mealbasket5,
+      mealbasket6,
+      mealbasket7,
+      mealbasket8,
+    ],
   },
   {
-    title: "Charity Action : Distribution of basic necessities (water, soap, blankets, medicine, food) to the homeless of Casablanca",
-    date: "January 2021",
-    desc: "",
-    images : [],
-  },
-  {
-    title: "Charity Action : Free tutoring for children in need from the association Heure Joyeuse.",
+    title:
+      "Charity Action : Free tutoring for children in need from the association Heure Joyeuse.",
     date: "Every Saturday",
     desc: "",
-    images : [],
+    images: [
+      freetutoring1,
+      freetutoring2,
+      freetutoring3,
+      freetutoring4,
+      freetutoring5,
+      freetutoring6,
+      freetutoring7,
+      freetutoring8,
+    ],
   },
   {
     title: "Ecological Action : Beach cleaning",
     date: "October 2021",
     desc: "",
-    images : [],
+    images: [
+      beachcleaning1,
+      beachcleaning2,
+      beachcleaning3,
+      beachcleaning4,
+      beachcleaning5,
+      beachcleaning6,
+      beachcleaning7,
+      beachcleaning8,
+    ],
   },
   {
-    title: "Awareness-raising : Short film about harassment and interview with a psychologist",
+    title:
+      "Awareness-raising : Short film about harassment and interview with a psychologist",
     date: "November 2021",
     desc: "",
-    images : [],
-  },
-  {
-    title: "Awareness-raising : Song about domestic violences",
-    date: "February 2022",
-    desc: "",
-    images : [],
-  },
-  {
-    title: "Fundraising : Sweatshirts sales",
-    date: "January 2022",
-    desc: "",
-    images : [],
-  },
-  {
-    title: "Fundraising : Roses and gifts sales for Valentine's Day",
-    date: "Every Year",
-    desc: "",
-    images : [],
-  },
+    images: [
+      shortfilm1,
+      shortfilm2,
+      shortfilm3,
+      shortfilm4,
+      shortfilm5,
+      shortfilm6,
+      shortfilm7,
+      shortfilm8,
+    ],
+  }
 ];
 
 export const productList = [
